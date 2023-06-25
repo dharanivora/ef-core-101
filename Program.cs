7033b84168c878a39a7068b95be33ffb44cb114a
Dharani Vora <dharanivora@outlook.com>
2023-06-25 11:11:19 -0400

    feat: register ContosoPizzaDbContext service into program class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ef_core_101.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddDbContext<ContosoPizzaContext>(options => options.UseSqlServer("Server=127.0.0.1,1433;Database=ef-core-101;User Id=sa;Password=&hq9%8Tb6X;TrustServerCertificate=True"));
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+var context = serviceProvider.GetServices<ContosoPizzaContext>();
