3ad0828c6ab9efe298dbdd91e50968cc704d92f4
Dharani Vora <dharanivora@outlook.com>
2023-06-25 10:59:10 -0400

    feat: implement Order model

--- Models/Order.cs
+++ Models/Order.cs
@@ -2,5 +2,16 @@ namespace ef_core_101.Models;
 
 public class Order
 {
+    public int Id { get; set; }
 
+    public DateTime OrderPlaced { get; set; }
+
+    public DateTime? OrderFulfilled { get; set; }
+
+    public int CustomrId { get; set; }
+
+    // This is called a "shadow property". We do not need to specify this because EF Core will automatically generate it.  
+    // public Customer Customer { get; set; } = default!;
+
+    public ICollection<OrderDetail> OrderDetails { get; set; } = default!;
 }
